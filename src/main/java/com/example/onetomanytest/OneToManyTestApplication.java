1ce24f7bb2b9b1846778677b8b1c5f4fa9da2a2d
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2020-01-04 11:05:49 +0630

    first commit

---
+++ src/main/java/com/example/onetomanytest/OneToManyTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.onetomanytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToManyTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneToManyTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onetomanytest/OneToManyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onetomanytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToManyTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
